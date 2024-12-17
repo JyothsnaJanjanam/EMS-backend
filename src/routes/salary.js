19c3ea4382d75f4b88114a27fbb6b5e7d58a8a75
JyothsnaJanjanam <janjanamjyothsna@gmail.com>
2024-12-17 21:44:16 +0530

    small-changes

--- src/routes/salary.js
+++ src/routes/salary.js
@@ -5,12 +5,12 @@ const { addSalary, getSalary } = require('../controllers/salary')
 
 const router = express.Router()
 
-router.use(
-  cors({
-    credentials: true,
-    'Access-Control-Allow-Origin': 'https://ems-frontend-iota.vercel.app/'
-  })
-)
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://ems-frontend-iota.vercel.app')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  next()
+})
 
 router.post('/add', verifyToken, addSalary)
 router.get('/:id/:role', verifyToken, getSalary)
--- src/routes/setting.js
+++ src/routes/setting.js
@@ -5,12 +5,12 @@ const { changePassword } = require('../controllers/setting')
 
 const router = express.Router()
 
-router.use(
-  cors({
-    credentials: true,
-    'Access-Control-Allow-Origin': 'https://ems-frontend-iota.vercel.app/'
-  })
-)
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://ems-frontend-iota.vercel.app')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  next()
+})
 
 router.put('/change-password', verifyToken, changePassword )
 
